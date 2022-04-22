032bcd35695ace14e43c51133664406d46ccf8fe
Luiz <luizfranzon05@hotmail.com>
2022-04-22 17:39:31 -0300

    string functions

---
+++ aulas/aula24/script.js
@@ -0,0 +1,19 @@
+let texto = "Um texto";
+//           01234567
+
+// console.log(texto.charAt(3))
+
+// console.log(texto.slice(3 ,6))
+
+// console.log(texto.concat(` em um lindo dia`))
+
+// console.log(texto.indexOf("texto"))
+
+// console.log(texto.lastIndexOf("texto"))
+
+// console.log(texto.replace("Um", "Dois"))
+
+// console.log(texto.split(' '))
+
+
+
